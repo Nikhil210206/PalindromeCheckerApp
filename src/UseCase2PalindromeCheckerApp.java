0d67c2d9c94f6f598e4817a6011a1d6acc37adfa
Nikhil B <nikhilbalamurugan@gmail.com>
2026-03-11 09:17:44 +0530

    Update UseCase2PalindromeCheckerApp.java

--- src/UseCase2PalindromeCheckerApp.java
+++ src/UseCase2PalindromeCheckerApp.java
@@ -1,60 +1,20 @@
 import java.util.Scanner;
 
-class Node {
-    char data;
-    Node next;
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+public class PalindromeUC10 {
 
-public class PalindromeUC8 {
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        int start = 0;
+        int end = str.length() - 1;
 
-    public static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null) return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        // Find middle using fast & slow pointers
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Reverse second half
-        Node secondHalf = reverse(slow.next);
-
-        Node firstHalf = head;
-        Node tempSecond = secondHalf;
-
-        boolean palindrome = true;
-        while (tempSecond != null) {
-            if (firstHalf.data != tempSecond.data) {
-                palindrome = false;
-                break;
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
             }
-            firstHalf = firstHalf.next;
-            tempSecond = tempSecond.next;
+            start++;
+            end--;
         }
-
-        // Restore original list (optional)
-        slow.next = reverse(secondHalf);
-
-        return palindrome;
-    }
-
-    private static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
+        return true;
     }
 
     public static void main(String[] args) {
@@ -62,14 +22,7 @@ public class PalindromeUC8 {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        Node head = new Node(input.charAt(0));
-        Node current = head;
-        for (int i = 1; i < input.length(); i++) {
-            current.next = new Node(input.charAt(i));
-            current = current.next;
-        }
-
-        if (isPalindrome(head)) {
+        if (isPalindrome(input)) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
