dc03795dd7b3f2ced3d29ea6ddff19ade7b7ae88
Nikhil B <nikhilbalamurugan@gmail.com>
2026-03-11 09:12:57 +0530

    Update UseCase2PalindromeCheckerApp.java

--- src/UseCase2PalindromeCheckerApp.java
+++ src/UseCase2PalindromeCheckerApp.java
@@ -1,27 +1,19 @@
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Scanner;
 
-public class PalindromeUC7 {
+public class PalindromeUC9 {
+
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+        if (str.charAt(start) != str.charAt(end)) return false;
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        Deque<Character> deque = new LinkedList<>();
-        for (char ch : input.toCharArray()) {
-            deque.addLast(ch);
-        }
-
-        boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
+        if (isPalindrome(input, 0, input.length() - 1)) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
