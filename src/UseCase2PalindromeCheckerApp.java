a95a38ab6bbeac9c735f70631b19882b3dcc621a
Nikhil B <nikhilbalamurugan@gmail.com>
2026-03-11 10:01:14 +0530

    Update UseCase2PalindromeCheckerApp.java

--- src/UseCase2PalindromeCheckerApp.java
+++ src/UseCase2PalindromeCheckerApp.java
@@ -1,14 +1,57 @@
-public class UseCase2PalindromeCheckerApp {
+import java.util.*;
+
+interface PalindromeStrategy {
+    boolean check(String text);
+}
+
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String text) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : text.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : text.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    public boolean check(String text) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : text.toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class PalindromeChecker {
+    private PalindromeStrategy strategy;
+
+    public PalindromeChecker(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean checkPalindrome(String text) {
+        return strategy.check(text);
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        String word = "Nikhil";
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
-        }
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
-        } else {
-            System.out.println(word + " is not a Palindrome.");
-        }
+        PalindromeChecker checker = new PalindromeChecker(new StackStrategy());
+        System.out.println(checker.checkPalindrome("madam"));
+
+        checker = new PalindromeChecker(new DequeStrategy());
+        System.out.println(checker.checkPalindrome("level"));
     }
 }
